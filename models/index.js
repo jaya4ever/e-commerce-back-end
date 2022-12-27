ad9e438c46afd1d07b605de797f9441dcecb163f
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-27 11:51:14 -0600

    debug the code in models category

--- models/index.js
+++ models/index.js
@@ -6,34 +6,34 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: {
-    name: 'product_id',
-    unique: false,
-  }
+
+  foreignKey: 'product_id'
+
+
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: {
-    name: 'category_id',
-    unique: false,
-  }
+
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(Tag, {
-  foreignKey: {
-    name: 'tag_id',
-    unique: false,
-  }
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+
+
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.hasMany(Product, {
-  foreignKey: {
-    name: 'tag_id',
-    unique: false,
-  }
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+
+
 });
 
 module.exports = {
